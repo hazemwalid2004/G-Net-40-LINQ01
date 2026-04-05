2302079d084d5b7599c4a22f0bb65691bab2c0a9
Hazem walid <walidf106@gmail.com>
2026-04-05 15:52:25 +0200

    Q01

--- Ass01LINQ/Program.cs
+++ Ass01LINQ/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace Ass01LINQ
+﻿using Session01LinqG01.Models;
+using static Session01LinqG01.DataSources.Source;
+namespace Ass01LINQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-
+            #region Q01
+            // Get all products from the "Seafood" category. Print each product's name and price.
+            var result = ProductList.Where(c => c.Category == "Seafood")
+                .Select(s=>new {name=s.ProductName,price=s.UnitPrice});
+            #endregion
+            foreach (var item in result) 
+            {
+                Console.WriteLine(item);
+            }
         }
+
     }
 }
