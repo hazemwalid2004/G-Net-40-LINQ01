17d79ca0b1d99153fd6ec1635ad7472deb50adbd
Hazem walid <walidf106@gmail.com>
2026-04-05 11:55:37 +0200

    Add project files.

---
+++ Ass01LINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ass01LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
